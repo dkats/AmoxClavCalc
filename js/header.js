f7f8ed2eb42d9d6ee208d25e727674211375a087
Dan Kats <dkats12@gmail.com>
2021-09-01 21:50:35 -0400

    Added header

--- js/header.js
+++ js/header.js
@@ -8,6 +8,7 @@ function header(level = 0) {
 	document.write('<header>');
 	document.write('<ul class="menu">');
 	document.write('	<li class="homeclick" onclick="window.open(\'' + dir + 'index.html\',\'_self\')" id="header_home">Amox-Clav Calculator</li>');
+	document.write('	<li onclick="window.open(\'' + dir + 'how.html\',\'_self\')" id="header_how">How Does It Work?</li>');
 	document.write('</ul>');
 	document.write('</header>');
 }
