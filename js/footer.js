c9a55478469a52858051b8538a0357e23844658d
Dan Kats <dkats12@gmail.com>
2021-09-01 21:50:06 -0400

    Changed email domain to amoxclavdosing.com

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,6 @@
 document.write('<footer>');
 document.write('<ul class="menu">');
 document.write('	<li class="noclick">&copy 2021 Daniel J. Kats</li>');
-document.write('	<li style="float:right;" onclick="window.open(\'mailto:contact@augmentinman.com\');">Contact</li>');
+document.write('	<li style="float:right;" onclick="window.open(\'mailto:contact@amoxclavdosing.com?subject=Feedback:+Amox-Clav+Calculator\');">Contact</li>');
 document.write('</ul>');
 document.write('</footer>');
