8077d75b9243fb94e20b30dbf2bde47d192854ca
Dan Kats <dkats12@gmail.com>
2021-11-19 10:14:52 -0500

    Added feature to demo the program by picking a random age, indication, and weight

--- js/amoxclav.js
+++ js/amoxclav.js
@@ -716,3 +716,34 @@ function refresh(listener) {
 
 	show();
 }
+
+// Sets a random age, indication, and weight
+function example() {
+	// Pick and set random indication (but don't pick the blank first option)
+	indication_el.selectedIndex = Math.floor(Math.random() * (indication_el.getElementsByTagName('option').length - 1) + 1);
+
+	// Pick random age
+	age_arr = ["<3", ">3"];
+	age_rand = age_arr[Math.floor(Math.random() * age_arr.length)];
+
+	// Pick and set random weight
+	if(age_rand == "<3") {
+		// If age <3 months, pick random weight between 2 kg and 8 kg
+		wt_el.value = round(Math.floor(Math.random() * 60) / 10 + 2,1);
+	} else {
+		// If age <3 months, randomly pick if the weight will be <40 kg or >40 kg
+		if(Math.floor(Math.random() * 2) == 0) {
+			// Pick random weight between 8 kg and 40 kg
+			wt_el.value = round(Math.floor(Math.random() * 320) / 10 + 8,1);
+		} else {
+			// Pick random weight between 40 kg and 80 kg
+			wt_el.value = round(Math.floor(Math.random() * 400) / 10 + 40,1);
+		}
+	}
+
+	// Set random age
+	age_el.value = age_rand;
+
+	refresh("indication");
+}
+
--- js/header.js
+++ js/header.js
@@ -9,6 +9,7 @@ function header(level = 0) {
 	document.write('<ul class="menu">');
 	document.write('	<li class="homeclick" onclick="window.open(\'' + dir + 'index.html\',\'_self\')" id="header_home">Amox-Clav Calculator</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'how.html\',\'_self\')" id="header_how">How Does It Work?</li>');
+	document.write('	<li onclick="example();" id="header_ex">Show An Example</li>');
 	document.write('</ul>');
 	document.write('</header>');
 }
