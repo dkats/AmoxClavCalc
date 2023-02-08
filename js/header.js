769b329188eb58f2fb6966f55d2cc0f23add4b2c
Dan Kats <dkats12@gmail.com>
2023-02-08 12:42:34 -0500

    Updated to 2023 and added citation information

--- js/header.js
+++ js/header.js
@@ -10,6 +10,7 @@ function header(level = 0) {
 	document.write('	<li class="homeclick" onclick="window.open(\'' + dir + 'index.html\',\'_self\')" id="header_home">Amox-Clav Calculator</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'how.html\',\'_self\')" id="header_how">How Does It Work?</li>');
 	document.write('	<li onclick="example();" id="header_ex">Show An Example</li>');
+	document.write('	<li onclick="window.open(\'' + dir + 'citation.html\',\'_self\')" id="header_citation">Citation Information</li>');
 	document.write('</ul>');
 	document.write('</header>');
 }
